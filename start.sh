9acdd7a0261fa62fb9abce5178ecd97feb4d6c90
Renishbot <117950358+Renishbot@users.noreply.github.com>
2022-12-05 16:25:23 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Natalie 
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Natalie 
-fi
-cd /Natalie 
+echo "Cloning koyeb Repository"
+git clone https://github.com/Renishbot/Natalie.git /Natalie 
+cd /Natalie
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 natalie.py
