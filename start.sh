12038698a5bcb7a48613728f1646100842aed1e8
Renishbot <117950358+Renishbot@users.noreply.github.com>
2023-01-04 19:02:49 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning main Repository"
-git clone https://github.com/Renishbot/Natalie 
+git clone https://github.com/Renishbot/Natalie.git /Natalie 
 cd /Natalie 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
